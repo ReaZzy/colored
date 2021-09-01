3519de12ea0a9ebef711e9ecd8b4a39584a3230e
REAZZY <maxim.nebela@gmail.com>
2021-09-01 19:46:02 +0300

    feat🔨: add validation

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,33 +3,47 @@ import { useDispatch } from 'react-redux';
 import { login } from '../../store/reducers/auth/thunks';
 import { useRouter } from 'next/router';
 import s from './login.module.css';
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+  find: yup.string().min(6).max(64).required(),
+  password: yup.string().min(6).max(64).required(),
+});
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
   const router = useRouter();
-  const handleSubmit = async (values: any) => {
+  const handleSubmit = async (values: { find: string; password: string }) => {
     await dispatch(await login(values.find, values.password));
     await router.push('/');
   };
   return (
-    <Formik onSubmit={handleSubmit} initialValues={{ find: '', password: '' }}>
-      <Form>
-        <label htmlFor={'find'}>Login or Email</label>
-        <Field
-          id={'find'}
-          name={'find'}
-          placeholder={'Enter your login or email'}
-        />
-        <label htmlFor={'password'}>Password</label>
-        <Field
-          id={'password'}
-          name={'password'}
-          placeholder={'Enter your password'}
-          type={'password'}
-        />
-        <button type={'submit'}>Login 🔐</button>
-      </Form>
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={{ find: '', password: '' }}
+      validationSchema={validationSchema}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <label htmlFor={'find'}>Login or Email</label>
+          <Field
+            id={'find'}
+            name={'find'}
+            placeholder={'Enter your login or email'}
+          />
+          <ErrorMessage name={'find'} />
+          <label htmlFor={'password'}>Password</label>
+          <Field
+            id={'password'}
+            name={'password'}
+            placeholder={'Enter your password'}
+            type={'password'}
+          />
+          <ErrorMessage name={'password'} />
+          <button type={'submit'}>Login 🔐</button>
+        </Form>
+      )}
     </Formik>
   );
 };
