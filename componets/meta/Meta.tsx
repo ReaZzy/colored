92f189b437f7364bf80774cafac5c44890d6e2a4
REAZZY <maxim.nebela@gmail.com>
2021-12-03 20:50:35 +0200

    feat🔨: add open graph

--- componets/meta/Meta.tsx
+++ componets/meta/Meta.tsx
@@ -3,11 +3,19 @@ import Head from 'next/head';
 
 interface IProps {
   title?: string;
+  image?: string;
+  url?: string;
+  description?: string;
 }
 
-export const Meta: NextPage<IProps> = ({ title }) => {
+export const Meta: NextPage<IProps> = ({ title, image, url, description }) => {
   return (
     <Head>
+      <meta property="og:title" content={`${title}`} />
+      <meta property="og:image" content={`${image}`} />
+      <meta property="og:url" content={`${url}`} />
+      <meta property="og:description" content={`${description}`} />
+      <meta property="og:type" content="website" />
       <title>{title || 'Colored'}</title>
     </Head>
   );
