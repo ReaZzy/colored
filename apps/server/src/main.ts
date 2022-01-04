5caa11ae0a49e5a62ad601ca134586e9586072a9
REAZZY <maxim.nebela@gmail.com>
2022-01-04 22:16:24 +0200

    fix🛠: remove cookie parser middleware

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
 import { graphqlUploadExpress } from 'graphql-upload';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({ credentials: true, origin: 'http://localhost:4200' });
-  app.use(cookieParser());
   app.use(graphqlUploadExpress());
 
   await app.listen(process.env.PORT);
